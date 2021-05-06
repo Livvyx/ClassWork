e0b02ce890a78f43ef2609f37e51f77d466379bb
Olivia Walker <79489152+Livvyx@users.noreply.github.com>
2021-05-06 16:45:50 +1200

    Add files via upload

--- StoringStudentsNamesAndGrades/StoringStudentsNamesAndGrades.cpp
+++ StoringStudentsNamesAndGrades/StoringStudentsNamesAndGrades.cpp
@@ -1,20 +1,75 @@
-// StoringStudentsNamesAndGrades.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+    // olivia walker
+
+    /* - storing names and grades of 3 students in the main function - */
+
+    //ALGORITHM
+
+    /*1. Input name and 3 grades
+      2. call find_sum(x,y,z) and store it in sum
+      3. call find_average(sum) and store it in average
+      4. call find_result(average) and output the result*/
 
 #include <iostream>
+#include <cstring>
+using namespace std;
+
+// SUM CALCULATION
+float find_sum(float p1, float p2, float p3) { 
+    float sum = p1 + p2 + p3;
+    return (sum);
+}
+
+// AVERAGE CALCULATION
+float find_average(float sum) {
+    return (sum / 3);
+}
+
+// OUTPUT FINAL RESULT TO USER
+void find_result(float average, string name) {
+    if (average >= 80 && average <= 100) {
+        cout << "Congratulations, " << name << ". You have earned a distinction!";
+    }
+    else if (average >= 50 && average < 80) {
+        cout << "Congratulations, " << name << ". You have passed!";
+    }
+    else if (average < 50 && average >= 0) {
+        cout << "Sorry, " << name << ". You have not earned enough points to pass.";
+    }
+    else {
+        cout << "//ERROR// Average grade was not between 0-100.";
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string student_name, result;
+    float paper1, paper2, paper3;
+    float sum;
+    float average;
+
+    // TAKING USER INPUT
+    cout << " Please enter your name: ";
+    cin >> student_name;
+    cout << " \n Enter students grades for the three papers: ";
+    cin >> paper1 >> paper2 >> paper3;
+
+    cout << "\n===================================================================";
+
+    // CALCULATING SUM
+    sum = find_sum(paper1, paper2, paper3);
+    cout << "\n SUM: " << sum;
+
+    // CALCULATING AVERAGE
+    average = find_average(sum);
+    cout << "\n GRADE AVERAGE AVERAGE: " << average;
+
+    // OUTPUTTING FINAL RESULT
+    cout << "\n FINAL GRADE: ";
+    find_result(average, student_name);
+
+    cout << "\n===================================================================\n";
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
